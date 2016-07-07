c0683088b241cf8d3bf5a9aa462fd5ebd30a6d47
lzxm160 <lzxm160@sina.com>
2016-07-07 14:18:07 +0800

    log name

--- src/log.hpp
+++ src/log.hpp
@@ -51,9 +51,10 @@ void init_log()
     setrlimit(RLIMIT_AS, &rl);
  
   muduo::Logger::setOutput(asyncOutput);
-  muduo::string name(get_config->m_log_name.c_str());
-  cout<<name<<endl;
-  muduo::AsyncLogging log(name, kRollSize);
+  cout<<get_config->m_log_name<<endl;
+  muduo::string file_name(get_config->m_log_name.c_str());
+ 
+  muduo::AsyncLogging log(file_name, kRollSize);
   log.start();
   g_asyncLog = &log;
 
