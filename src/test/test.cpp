e2f6bee59c5436cddc43c429484cba852bf3e137
lzxm160 <lzxm160@sina.com>
2016-07-12 11:40:55 +0800

    test deadline timer

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -106,7 +106,7 @@ namespace test1
 	        	}
 	        	
 	        	//cout<<"handle wait"<<endl;
-	            m_d_t.expires_from_now(boost::posix_time::seconds(10);  
+	            m_d_t.expires_from_now(boost::posix_time::seconds(10));  
 	            m_d_t.async_wait(boost::bind(&exchange_rate_on_time::handle_wait_,shared_from_this(), boost::asio::placeholders::error));                 
 	    	}   
 		}  
