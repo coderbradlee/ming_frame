32bbd5d8d21c1a03860eb025e8ccf656a396d572
lzxm160 <lzxm160@sina.com>
2016-07-20 16:26:18 +0800

    delete release info

--- Build/release/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/ming_frame
-# Build directory: /root/ming_frame/Build/release
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(muduo/base)
-SUBDIRS(muduo/net)
-SUBDIRS(src)
