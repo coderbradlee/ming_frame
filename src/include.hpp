0cf1082d398652967ec60bbb8b04d75bea02ef08
lzxm160 <lzxm160@sina.com>
2016-07-22 10:13:35 +0800

    test thread safe observer

--- src/include.hpp
+++ src/include.hpp
@@ -41,6 +41,7 @@
 #include <memory>
 #include <boost/enable_shared_from_this.hpp>  
 #include <boost/shared_ptr.hpp>  
+#include <boost/weak_ptr.hpp> 
 #include <boost/bind.hpp>  
 #include <boost/function.hpp>  
 #include <boost/asio.hpp>
