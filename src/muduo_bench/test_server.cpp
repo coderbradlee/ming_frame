41e8cac3400091ada705870760993a7724945411
lzxm160 <lzxm160@sina.com>
2016-07-26 11:12:21 +0800

    test muduo discard server

---
+++ src/muduo_bench/test_server.cpp
@@ -0,0 +1,42 @@
+#include <muduo/net/TcpServer.h>
+
+#include <muduo/base/Atomic.h>
+#include <muduo/base/Logging.h>
+#include <muduo/base/Thread.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/InetAddress.h>
+
+#include <boost/bind.hpp>
+
+#include <utility>
+
+#include <mcheck.h>
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace muduo;
+using namespace muduo::net;
+
+#include "test_server.h"
+int main(int argc, char* argv[])
+{
+  LOG_INFO << "pid = " << getpid();
+  EventLoop loop;  // one loop shared by multiple servers
+
+  // ChargenServer chargenServer(&loop, InetAddress(2019));
+  // chargenServer.start();
+
+  // DaytimeServer daytimeServer(&loop, InetAddress(2013));
+  // daytimeServer.start();
+
+  DiscardServer discardServer(&loop, InetAddress(2009));
+  discardServer.start();
+
+  // EchoServer echoServer(&loop, InetAddress(2007));
+  // echoServer.start();
+
+  // TimeServer timeServer(&loop, InetAddress(2037));
+  // timeServer.start();
+
+  loop.loop();
+}
---
+++ src/muduo_bench/test_server.hpp
@@ -0,0 +1,58 @@
+#ifndef TEST_SERVER_HPP
+#define TEST_SERVER_HPP
+
+#include <muduo/net/TcpServer.h>
+
+class DiscardServer
+{
+public:
+  DiscardServer(muduo::net::EventLoop* loop,
+             const muduo::net::InetAddress& listenAddr):server_(loop,listenAddr,"discardServer")
+  {
+  	server_.setConnectionCallback(
+      boost::bind(&DiscardServer::onConnection, this, _1));
+    server_.setMessageCallback(
+      boost::bind(&DiscardServer::onMessage, this, _1, _2, _3));
+  }
+  void start()
+  {
+  	server_.start();
+  }
+private:
+	void onConnection(const muduo::net::TcpConnectionPtr& conn)
+	{
+		LOG_INFO<<"DiscardServer - " << conn->peerAddress().toIpPort() << " -> "
+           << conn->localAddress().toIpPort() << " is "
+           << (conn->connected() ? "UP" : "DOWN");
+	}
+
+    void onMessage(const muduo::net::TcpConnectionPtr& conn,
+                 muduo::net::Buffer* buf,
+                 muduo::Timestamp time)
+    {
+    	string msg(buf->retrieveAllAsString());
+    	LOG_INFO<<conn->name()<<" discards "<<msg.size()<<" "<<time.toString();
+    }
+
+  muduo::net::TcpServer server_;
+};
+// RFC 868
+class TimeServer
+{
+ public:
+  TimeServer(muduo::net::EventLoop* loop,
+             const muduo::net::InetAddress& listenAddr);
+
+  void start();
+
+ private:
+  void onConnection(const muduo::net::TcpConnectionPtr& conn);
+
+  void onMessage(const muduo::net::TcpConnectionPtr& conn,
+                 muduo::net::Buffer* buf,
+                 muduo::Timestamp time);
+
+  muduo::net::TcpServer server_;
+};
+
+#endif  // MUDUO_EXAMPLES_SIMPLE_TIME_TIME_H
