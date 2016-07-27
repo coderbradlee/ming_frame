5a68fd8408043401a49d5160ba7d2f0efa4b2f36
lzxm160 <lzxm160@sina.com>
2016-07-27 16:41:04 +0800

    test muduo roundtrip

--- src/test/test1.hpp
+++ src/test/test1.hpp
@@ -6,7 +6,7 @@ namespace test1
 {
 	namespace test_round_trip
 	{
-		typedef boost::shared_ptr<TcpConnection> TcpConnectionPtr;
+		typedef boost::shared_ptr<muduo::net::TcpConnection> TcpConnectionPtr;
 		void serverConnectionCallback(const TcpConnectionPtr& conn);
 
 		void serverMessageCallback(const TcpConnectionPtr& conn,
