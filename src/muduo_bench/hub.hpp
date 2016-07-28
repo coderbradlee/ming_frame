775d76c051c286fdb68db25dff131398659fac9c
lzxm160 <lzxm160@sina.com>
2016-07-28 13:21:41 +0800

    test muduo hub

--- src/muduo_bench/hub.hpp
+++ src/muduo_bench/hub.hpp
@@ -13,8 +13,8 @@ class topic:public muduo::copyable
 {
 public:
   topic(const muduo::string& topic);
-  void add(const TcpConnectionPtr& conn);
-  void remove(const TcpConnectionPtr& conn);
+  void add(const muduo::net::TcpConnectionPtr& conn);
+  void remove(const muduo::net::TcpConnectionPtr& conn);
   void publish(const muduo::string& content,Timestamp time);
 private:
   muduo::string make_message();
@@ -22,7 +22,7 @@ private:
   muduo::string m_topic;
   muduo::string m_content;
   Timestamp m_last_pubtime;
-  std::set<TcpConnectionPtr> m_audiences;
+  std::set<muduo::net::TcpConnectionPtr> m_audiences;
 };
 class hub:boost::noncopyable
 {
@@ -30,11 +30,11 @@ public:
   hub(muduo::net::EventLoop* loop,const muduo::net::InetAddress& addr);
   void start();
 private:
-  void on_connection(const TcpConnectionPtr& conn);
-  void on_message(const TcpConnectionPtr&conn,muduo::net::Buffer* buf,Timestamp receive_time);
+  void on_connection(const muduo::net::TcpConnectionPtr& conn);
+  void on_message(const muduo::net::TcpConnectionPtr&conn,muduo::net::Buffer* buf,Timestamp receive_time);
   void time_publish();
-  void subscribe(const TcpConnectionPtr& conn,const muduo::string& topic);
-  void unsubscribe(const TcpConnectionPtr&conn,const muduo::string& topic);
+  void subscribe(const muduo::net::TcpConnectionPtr& conn,const muduo::string& topic);
+  void unsubscribe(const muduo::net::TcpConnectionPtr&conn,const muduo::string& topic);
   void publish(const muduo::string& source,const muduo::string& topic,const muduo::string& content,Timestamp time);
   topic& get_topic(const muduo::string& topic);
 private:
