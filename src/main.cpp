08dba2b588e762f4a16de0b4d2c3e2e6a46a245c
lzxm160 <lzxm160@sina.com>
2016-08-03 10:18:26 +0800

    add boost::program_options

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,54 @@
+#include <boost/program_options.hpp>
 #include "log.hpp"
 #include "fastcgi/fastcgi_resource.h"
 int start_inspector_thread();
+namespace po = boost::program_options; 
+bool parseCommandLine(int argc, char* argv[], Options* opt)
+{
+  po::options_description general("general options");  
+    general.add_options()  
+        ("help,h", "produce help message");
+  
+    string config_file;  
+    po::options_description config("config options");  
+    config.add_options()  
+        ("config", po::value<string>(&config_file)->default_value("config.ini"),  
+        "set config file, specified with '@name' too");  
+  
+    po::options_description all("All options");  
+    all.add(general).add(config);  
+  
+    po::variables_map vm;  
+    po::store(po::command_line_parser(argc, argv).options(all).extra_parser(::at_option_parser).run(), vm);   
+  
+    if (vm.count("help"))  
+    {  
+        cout << general << endl;  
+        cout << config << endl;  
+        return false;  
+    }  
+  
+    if (vm.count("config"))  
+    {  
+        string conf_name = vm["config"].as<string>();  
+        ifstream ifs_config(conf_name.c_str());  
+        boost::shared_ptr<iconfig> get_config= iconfig::get_instance(conf_name);
+        // if (! ifs_config)  
+        // {  
+        //     cerr << "could not open the configure file" << endl;  
+        //     return false;  
+        // }  
+  
+        // stringstream ss_config;  
+        // ss_config << ifs_config.rdbuf();  
+  
+        // global::strings_t args;  
+        // global::separate_tokens(ss_config.str(), args, " \r\n");  
+        // po::store(po::command_line_parser(args).options(all).run(), vm);  
+    }  
+    po::notify(vm);  
+  
+}
 int main()
 {
   using std::string;
@@ -8,6 +56,11 @@ int main()
   using std::endl;
   try
   {
+    Options options;
+    if (parseCommandLine(argc, argv, &options))
+    {
+
+    }
     init_log();
     muduo::Timestamp start = muduo::Timestamp::now();
 
