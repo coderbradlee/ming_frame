9a06bcf32b6ac39aa2104fb4f577f09c7a39e469
lzxm160 <lzxm160@sina.com>
2016-08-10 09:15:24 +0800

    add popen

--- src/procmon.hpp
+++ src/procmon.hpp
@@ -245,7 +245,7 @@ private:
     if (!pp) {
         return "error";
     }
-    char tmp[1024]; //设置一个合适的长度，以存储每一行输出
+    char tmp[1024]={0}; //设置一个合适的长度，以存储每一行输出
     while (fgets(tmp, sizeof(tmp), pp) != NULL) {
         // if (tmp[strlen(tmp) - 1] == '\n') {
         //     tmp[strlen(tmp) - 1] = '\0'; //去除换行符
