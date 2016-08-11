43577be8da46b15705e096a23456d65cd59f5d6b
lzxm160 <lzxm160@sina.com>
2016-08-11 17:12:19 +0800

    intrusive ptr

--- src/include.hpp
+++ src/include.hpp
@@ -43,6 +43,7 @@
 #include <boost/enable_shared_from_this.hpp>  
 #include <boost/shared_ptr.hpp>  
 #include <boost/weak_ptr.hpp> 
+#include <boost/intrusive_ptr.hpp> 
 #include <boost/bind.hpp>  
 #include <boost/function.hpp>  
 #include <boost/asio.hpp>
