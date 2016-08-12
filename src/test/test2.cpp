6f25f9e411374be33c6d00daa0f9ef6d0879ee58
lzxm160 <lzxm160@sina.com>
2016-08-12 14:59:06 +0800

    test2

--- src/test/test2.cpp
+++ src/test/test2.cpp
@@ -3,10 +3,53 @@ namespace test2_namespace
 {
 	using muduo::string;
 	
-
+	namespace thread_safe_observable
+	{
+		
+		void observable::register(boost::weak_ptr<observer> x)
+		{
+			m_observers.push_back(x);
+		}
+		void observable::notify()
+		{
+			muduo::MutexLockGuard lock(m_mutex);
+			m_observers.erase(std::remove_if(m_observers.begin(),m_observers.end(),[](boost::weak_ptr<observer> x)
+			{
+				boost::shared_ptr<observer> obj(x.lock());
+				if(obj)
+				{
+					obj->update();
+					return false;
+				}
+				return true;
+			}),m_observers.end());
+		}
+		
+		
+		observer::~observer()
+		{
+			std::cout<<"~observer"<<std::endl;
+		}
+		void observer::update()
+		{
+			std::cout<<"update"<<std::endl;
+		}
+		void observer::observe(observable* s)
+		{
+			s->register(shared_from_this());
+			m_observable=s;
+		}
+	
+	}
 	void test_out()
 	{
-
+		observable able;
+		{
+			boost::shared_ptr<observer> p(new observer());
+			p->observe(&able);
+			able->notify();
+		}
+		able.notify();
 	}
 	
 }
--- src/test/test2.hpp
+++ src/test/test2.hpp
@@ -4,6 +4,28 @@
 #include "../include.hpp"
 namespace test2_namespace
 {
+	namespace thread_safe_observable
+	{
+		class observer;
+		class observable
+		{
+		public:
+			void register(boost::weak_ptr<observer> x);
+			void notify();
+		private:
+			mutable MutexLock m_mutex;
+			std::vector<boost::weak_ptr<observer>> m_observers;
+		};
+		class observer:public boost::enable_shared_from_this<observer>
+		{
+		public:
+			virtual ~observer();
+			virtual void update();
+			void observe(observable* s);
+		private:
+			observable* m_observable;
+		};
+	}
 	void test_out();
 }
 #endif
