bcf9b643c530a51d04da1cff4b3527afc2bdaaeb
lzxm160 <lzxm160@sina.com>
2016-08-12 09:54:53 +0800

    intrusive ptr

--- src/test/test1.cpp
+++ src/test/test1.cpp
@@ -66,7 +66,7 @@ namespace test1_namespace
 				std::cout<<"~B"<<std::endl;
 			}
 		private:
-			int x;
+			char* x;
 		};
 		/////////////////////////////////////
 		class A:public reference_couter
