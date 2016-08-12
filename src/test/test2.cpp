29b5fffc4f5d46b4e28196bb63d7fd9c8020bcea
lzxm160 <lzxm160@sina.com>
2016-08-12 16:52:28 +0800

    observer with thread safe

--- src/test/test2.cpp
+++ src/test/test2.cpp
@@ -2,7 +2,74 @@
 namespace test2_namespace
 {
 	using muduo::string;
+	namespace test_weak_call_back
+	{
 	
+		stock::stock(const string& name):m_name(name)
+		{
+			std::cout<<"constructor:"<<this<<":"<<name<<std::endl;
+		}
+		stock::~stock()
+		{
+			std::cout<<"destructor:"<<this<<":"<<name<<std::endl;
+		}
+		const string& stock::key()const
+		{
+			return m_name;
+		}
+		
+		boost::shared_ptr<stock> stock_factory::get(const string& key)
+		{
+			boost::shared_ptr<stock> p;
+			muduo::MutexLockGuard lock(m_mutex);
+			boost::weak_ptr<stock>& w=m_stocks[key];
+			p=w.lock();
+			if(!p)
+			{
+				p.reset(new stock(key),boost::bind(&stock_factory::weak_call_back,boost::weak_ptr(shared_from_this()),_1));
+				w=p;
+			}
+			return p;
+		}
+	
+		static stock_factory::void weak_call_back(const boost::weak_ptr<stock_factory>& w,stock* s)
+		{
+			boost::shared_ptr<stock_factory> f(w.lock());
+			if(f)
+			{
+				f->remove_stock(s);
+			}
+			delete s;
+		}
+		void stock_factory::remove_stock(stock* s)
+		{
+			if(s)
+			{
+				muduo::MutexLockGuard lock(m_mutex);
+				m_stocks.erase(s->key());
+			}
+		}
+	
+		void test()
+		{
+			{
+				boost::shared_ptr<stock_factory> f(new stock_factory());
+				{
+					boost::shared_ptr<stock> s1=f->get("ibm");
+
+					boost::shared_ptr<stock> s2=f->get("ibm");
+				}
+			}
+			{
+				boost::shared_ptr<stock> s;
+				{
+					boost::shared_ptr<stock_factory> f(new stock_factory());
+					s=f->get("ibm");
+					boost::shared_ptr<stock> s2=f->get("ibm");
+				}
+			}
+		}
+	}
 	namespace thread_safe_observable
 	{
 		
--- src/test/test2.hpp
+++ src/test/test2.hpp
@@ -27,6 +27,31 @@ namespace test2_namespace
 			std::vector<boost::weak_ptr<observer>> m_observers;
 		};
 	}
+	namespace test_weak_call_back
+	{
+		class stock
+		{
+		public:
+			stock(const string& name);
+			~stock();
+			const string& key()const;
+		private:
+			string m_name;
+		};
+		class stock_factory:public boost::enable_shared_from_this<stock_factory>
+		{
+		public:
+			boost::shared_ptr<stock> get(const string& key);
+		private:
+			static void weak_call_back(const boost::weak_ptr<stock_factory>,stock* s);
+			void remove_stock(stock* s);
+		private:
+			mutable muduo::MutexLock m_mutex;
+			std::map<string,boost::weak_ptr<stock>> m_stocks;
+		};
+
+		void test();
+	}
 	void test_out();
 }
 #endif
