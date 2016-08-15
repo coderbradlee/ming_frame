99cb78f8163dd5e67d13548a0ae3bb12089111c3
lzxm160 <lzxm160@sina.com>
2016-08-15 17:56:47 +0800

    month report

--- src/month_report/month_report.cpp
+++ src/month_report/month_report.cpp
@@ -19,7 +19,7 @@ void month_report::deal_with_sales_info()
 			boost::scoped_ptr< sql::PreparedStatement > prep_stmt(m_con->prepareStatement(query_string));
 			boost::scoped_ptr< sql::ResultSet > res(prep_stmt->executeQuery());
 
-			//query_string="select employee_no from t_system_account where system_account_id='"+res->getString("sales_id")+"'";
+			query_string="select employee_no from t_system_account where system_account_id='"+res->getString(1)+"'";
 		}
 		// {
 		// 	boost::scoped_ptr< sql::ResultSet > res(m_pstmt->executeQuery(query_string));
