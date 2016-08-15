8ac2b57631a15a16aeb9d464fd9f35216c477ea2
lzxm160 <lzxm160@sina.com>
2016-08-15 16:20:31 +0800

    month report

--- src/month_report/month_report.cpp
+++ src/month_report/month_report.cpp
@@ -7,31 +7,12 @@ month_report::month_report(boost::shared_ptr<mysql_info_> in)
 	
 	m_con->setSchema(in->database);
 }
-void month_report::start()
+void month_report::insert_data()
 {
 	try
 	{
-	std::string query_string=
-	"SELECT
-	t_quotation.quotation_id as quotation_id,
-	t_quotation_detail.quotation_detail_id as quotation_detail_id,
-	t_quotation.quotation_no as quotation_no,
-	t_quotation_detail.quantity as product_qty_pc,
-	t_quotation_detail.ss_guidance_price as guidance_price,
-	t_quotation_detail.module_extend_prop as module_extend_prop,
-	t_quotation_detail.applied_unit_price as unit_price,
-	t_quotation.createAt as creat_at
-	FROM
-	t_quotation INNER JOIN
-	t_quotation_detail
-	on
-	t_quotation_detail.quotation_id = t_quotation.quotation_id
-	and t_quotation_detail.dr=0 and t_quotation.createAt like "2016-07%"
-	ORDER BY t_quotation.createAt";
-	query(query_string);
-	std::cout<<__FILE__<<":"<<__LINE__<<std::endl;
-	while (m_res->next()) 
-    {
+	while(m_res->next())
+	{
 		std::cout<< m_res->getString(1)<<":" << m_res->getString("product_qty_pc")<<std::endl;
 	 	boost::shared_ptr<report_data> temp(new report_data());
 	 	temp->quotation_id=m_res->getString("quotation_id");
@@ -51,7 +32,51 @@ void month_report::start()
 	 	temp->creat_at=m_res->getString("creat_at");
 	 	m_report_datas.insert(temp);
 	 	std::for_each(m_report_datas.begin(),m_report_datas.end(),[](boost::shared_ptr<report_data>& x){x.print();});
-    }
+	}
+	}
+	catch (sql::SQLException &e) 
+	{
+	  //ming_log->get_log_console()->info()<< "# ERR: " << e.what();
+	  //ming_log->get_log_console()->info()<< " (MySQL error code: " << e.getErrorCode();
+	  //ming_log->get_log_console()->info()<< ", SQLState: " << e.getSQLState();
+	  LOG_ERROR<<"# ERR: " << e.what();
+	  LOG_ERROR<<" (MySQL error code: " << e.getErrorCode();
+	  LOG_ERROR<<", SQLState: " << e.getSQLState();
+
+	}
+	catch (std::exception& e)
+  	{
+    	LOG_ERROR<<"# ERR: " << e.what();
+  	}
+  	catch (...)
+  	{
+    	LOG_ERROR<<"unknown error ";
+  	}
+}
+void month_report::start()
+{
+	try
+	{
+	std::string query_string=
+	"SELECT\
+	t_quotation.quotation_id as quotation_id,\
+	t_quotation_detail.quotation_detail_id as quotation_detail_id,\
+	t_quotation.quotation_no as quotation_no,\
+	t_quotation_detail.quantity as product_qty_pc,\
+	t_quotation_detail.ss_guidance_price as guidance_price,\
+	t_quotation_detail.module_extend_prop as module_extend_prop,\
+	t_quotation_detail.applied_unit_price as unit_price,\
+	t_quotation.createAt as creat_at\
+	FROM\
+	t_quotation INNER JOIN\
+	t_quotation_detail\
+	on\
+	t_quotation_detail.quotation_id = t_quotation.quotation_id\
+	and t_quotation_detail.dr=0 and t_quotation.createAt like \"2016-07%\"\
+	ORDER BY t_quotation.createAt";
+	query(query_string);
+	std::cout<<__FILE__<<":"<<__LINE__<<std::endl;
+	insert_data();
    } 
 	catch (sql::SQLException &e) 
 	{
