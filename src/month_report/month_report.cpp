31fda6e4b4e0607bed9103e5ef91fe182ceee1d0
lzxm160 <lzxm160@sina.com>
2016-08-16 08:13:21 +0800

    month report

--- src/month_report/month_report.cpp
+++ src/month_report/month_report.cpp
@@ -18,15 +18,15 @@ void month_report::deal_with_sales_info()
 		std::cout<<query_strings<<":"<<__FILE__<<":"<<__LINE__<<std::endl;
 
 		{
-			// boost::scoped_ptr< sql::Statement > stmt(m_con->createStatement());
-			// boost::scoped_ptr< sql::ResultSet > res(stmt->executeQuery(query_strings));
+			boost::scoped_ptr< sql::Statement > stmt(m_con->createStatement());
+			boost::scoped_ptr< sql::ResultSet > res(stmt->executeQuery(query_strings));
 			// boost::scoped_ptr< sql::PreparedStatement > prep_stmt(m_con->prepareStatement(query_string));
 			// boost::scoped_ptr< sql::ResultSet > res(prep_stmt->executeQuery());
 			
 			// query_strings="select employee_no from t_system_account where system_account_id='"+c_string+"'";
-			query(query_strings);
+			
 			std::cout<<":"<<__FILE__<<":"<<__LINE__<<std::endl;
-			std::string c_string = m_res->getString(1).asStdString();
+			std::string c_string = res->getString(1).asStdString();
 			std::cout<<c_string<<":"<<__FILE__<<":"<<__LINE__<<std::endl;
 			//query_strings="select employee_no from t_system_account where system_account_id='"+m_res->getString(1).asStdString()+"'";
 			//std::cout<<query_strings<<":"<<__FILE__<<":"<<__LINE__<<std::endl;
