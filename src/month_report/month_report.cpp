f062468cfa1e0be9eb726c7a98b0c0b82f15063c
lzxm160 <lzxm160@sina.com>
2016-08-16 10:59:58 +0800

    month report

--- src/month_report/month_report.cpp
+++ src/month_report/month_report.cpp
@@ -17,7 +17,12 @@ void month_report::deal_with_payment_method_info()
 		std::cout<<query_string<<":"<<__FILE__<<":"<<__LINE__<<std::endl;
 		query(query_string);
 		m_res->next();
-		if(m_res->isNull("payment_method_id")||m_res->getString(1)=="") 
+		if(m_res->isNull("payment_method_id")) 
+		{
+			std::cout<<__FILE__<<":"<<__LINE__<<std::endl;
+			continue;
+		}
+		else if(m_res->getString(1)=="")
 		{
 			std::cout<<__FILE__<<":"<<__LINE__<<std::endl;
 			continue;
