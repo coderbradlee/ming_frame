c9376965e6495aeee63d0908d417effd7037283b
lzxm160 <lzxm160@sina.com>
2016-08-16 08:05:45 +0800

    month report

--- src/month_report/month_report.cpp
+++ src/month_report/month_report.cpp
@@ -22,11 +22,13 @@ void month_report::deal_with_sales_info()
 			// boost::scoped_ptr< sql::ResultSet > res(stmt->executeQuery(query_strings));
 			// boost::scoped_ptr< sql::PreparedStatement > prep_stmt(m_con->prepareStatement(query_string));
 			// boost::scoped_ptr< sql::ResultSet > res(prep_stmt->executeQuery());
-			// std::string c_string = res->getString(1);//.asStdString();
+			
 			// query_strings="select employee_no from t_system_account where system_account_id='"+c_string+"'";
 			query(query_strings);
+			std::string c_string = m_res->getString(1);//.asStdString();
+			std::cout<<c_string<<":"<<__FILE__<<":"<<__LINE__<<std::endl;
 			//query_strings="select employee_no from t_system_account where system_account_id='"+m_res->getString(1).asStdString()+"'";
-			std::cout<<query_strings<<":"<<__FILE__<<":"<<__LINE__<<std::endl;
+			//std::cout<<query_strings<<":"<<__FILE__<<":"<<__LINE__<<std::endl;
 			
 		}
 		// {
