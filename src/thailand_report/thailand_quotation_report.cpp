e09ad8bb9082217530e7160da13adca16b83a9a0
lzxm160 <lzxm160@sina.com>
2016-08-19 16:04:38 +0800

    thailand quotation report

--- src/thailand_report/thailand_quotation_report.cpp
+++ src/thailand_report/thailand_quotation_report.cpp
@@ -553,7 +553,7 @@ void month_report::insert_data()
 }
 void month_report::write_to_csv()
 {
-	write_csv w("thailand_report.csv");
+	write_csv w("thailand_quotation_report.csv");
 	std::for_each(m_report_datas.begin(),m_report_datas.end(),[&](boost::shared_ptr<report_data>& x)
 		{
 			if(x->sales_company_name=="ReneSola Thailand")
