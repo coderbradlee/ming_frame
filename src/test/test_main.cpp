4ae1aa9a96780ac0003c0b0565a4bb2cf9b3f334
lzxm160 <lzxm160@sina.com>
2016-09-18 16:12:43 +0800

    bound

--- src/test/test_main.cpp
+++ src/test/test_main.cpp
@@ -23,40 +23,40 @@ int solution(const std::vector<int>& nums)
     ret.push_back(bound(nums[0],nums[0]));
     std::cout<<__LINE__<<std::endl;
     int max=0;
-    // for(const auto& num:nums)
-    // {
-    //     for(auto it=ret.begin();it!=ret.end();++it)
-    //     {
-    //         if(num==(it->high+1))
-    //         {
-    //             it->high=num;
-    //         }
-    //         else if(num==(it->low-1))
-    //         {
-    //             it->low=num;
-    //         } 
-    //         else
-    //         {
-    //             ret.push_back(bound(num,num));
-    //         }    
-    //         if(it->high-it->low+1>max)
-    //             max=it->high-it->low+1;
+    for(const auto& num:nums)
+    {
+        for(auto it=ret.begin();it!=ret.end();++it)
+        {
+            if(num==(it->high+1))
+            {
+                it->high=num;
+            }
+            else if(num==(it->low-1))
+            {
+                it->low=num;
+            } 
+            else
+            {
+                ret.push_back(bound(num,num));
+            }    
+            if(it->high-it->low+1>max)
+                max=it->high-it->low+1;
 
-    //     }
-    // }
-    // std::cout<<__LINE__<<std::endl;
-    // for(const auto& n:ret)
-    // {
-    //     std::cout<<n.high<<":"<<n.low<<std::endl;
-    // }
+        }
+    }
+    std::cout<<__LINE__<<std::endl;
+    for(const auto& n:ret)
+    {
+        std::cout<<n.high<<":"<<n.low<<std::endl;
+    }
     return max;
 }
 void foo()
 {
-    std::cout<<__LINE__<<std::endl;
+    //std::cout<<__LINE__<<std::endl;
     std::vector<int> nums{6,4,8,1,3,2,7};
 
-    std::cout<<__LINE__<<std::endl;
+    //std::cout<<__LINE__<<std::endl;
 
     std::cout<<solution(nums)<<std::endl;
 }
