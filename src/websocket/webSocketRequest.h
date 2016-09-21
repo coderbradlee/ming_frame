986ea49eec3d6e23d27668e63d979b8d1e152799
lzxm160 <lzxm160@sina.com>
2016-09-21 16:51:01 +0800

    websocket

--- src/websocket/webSocketRequest.h
+++ src/websocket/webSocketRequest.h
@@ -182,7 +182,7 @@ class webSocketRequest : public muduo::copyable
   string query_;
   Timestamp receiveTime_;
   std::map<string, string> headers_;
-  const std::string ws_magic_string="258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+  const string ws_magic_string="258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 };
 
 }
