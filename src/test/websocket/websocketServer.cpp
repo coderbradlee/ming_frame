d17bf9836db3eb49feb56f2cc6aa6c9c69686986
lzxm160 <lzxm160@sina.com>
2016-09-29 09:25:37 +0800

    websocket

--- src/test/websocket/websocketServer.cpp
+++ src/test/websocket/websocketServer.cpp
@@ -13,11 +13,11 @@ void websocketServer::start()
 
 void websocketServer::onConnection(const muduo::net::TcpConnectionPtr& conn)
 {
-	LOG_INFO<<conn->peerAddress().toIpPort()<<"->"<<conn->localAddress().toIpPort()<<" is"<<(conn->connected()?"up":"down");
+	//LOG_INFO<<conn->peerAddress().toIpPort()<<"->"<<conn->localAddress().toIpPort()<<" is "<<(conn->connected()?"up":"down");
 }
 void websocketServer::onMessage(const muduo::net::TcpConnectionPtr& conn,muduo::net::Buffer* buf,muduo::Timestamp time)
 {
 	muduo::string msg(buf->retrieveAllAsString());
-	LOG_INFO<<conn->name()<<":"<<msg.size()<<",time:"<<time.toString();
-	
+	//LOG_INFO<<conn->name()<<":"<<msg.size()<<",time:"<<time.toString();
+	LOG_INFO<<msg;
 }
