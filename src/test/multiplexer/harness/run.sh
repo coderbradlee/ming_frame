c737cea6fb6cbcaca22c1e373f7de6356549fe34
lzxm160 <lzxm160@sina.com>
2016-10-11 11:54:14 +0800

    multiplexer

--- src/test/multiplexer/harness/run.sh
+++ src/test/multiplexer/harness/run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-CLASSPATH=lib/netty-3.2.4.Final.jar:lib/slf4j-api-1.6.1.jar:lib/slf4j-simple-1.6.1.jar:./bin
-
+CLASSPATH=/usr/share/java/netty/netty-all-4.1.5.Final.jar:/usr/share/java/slf4j/slf4j-api.jar:lib/slf4j-simple.jar:/usr/share/java
 export CLASSPATH
 mkdir -p bin
 javac -d bin ./src/com/chenshuo/muduo/example/multiplexer/*.java ./src/com/chenshuo/muduo/example/multiplexer/testcase/*.java
