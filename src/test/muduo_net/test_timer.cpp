f36646fe141ec7278011e27799c8863cd907a784
lzxm160 <lzxm160@sina.com>
2016-10-13 14:08:59 +0800

    test muduo net timer

--- src/test/muduo_net/test_timer.cpp
+++ src/test/muduo_net/test_timer.cpp
@@ -23,18 +23,18 @@ int main(int argc, char* argv[])
   muduo::Logger::setLogLevel(muduo::Logger::LogLevel::TRACE);
   EventLoop loop;
   g_loop = &loop;
-  int timerfd=timerfd_create(CLOCK_MONOTONIC,TFD_NONBLOCK|TFD_CLOEXEC);
+  int timerfd=::timerfd_create(CLOCK_MONOTONIC,TFD_NONBLOCK|TFD_CLOEXEC);
   muduo::net::Channel channel(&loop,timerfd);
   channel.setReadCallback(timeout_func);
   channel.enableReading();
   struct itimerspec howlong;
   bzero(&howlong,sizeof howlong);
   howlong.it_value.tv_sec=5;
-  timerfd_settime(timerfd,0,&howlong,NULL);
+  ::timerfd_settime(timerfd,0,&howlong,NULL);
 
   // muduo::Thread t([&](){g_loop->loop();});
   // t.start();
   // t.join();
   g_loop->loop();
-  close(timerfd);
+  ::close(timerfd);
 }
