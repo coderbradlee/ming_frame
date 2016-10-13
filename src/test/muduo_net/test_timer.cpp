130f732f2d8b10e47ec2a7eba23d501640837355
lzxm160 <lzxm160@sina.com>
2016-10-13 13:53:20 +0800

    test muduo net timer

---
+++ src/test/muduo_net/test_timer.cpp
@@ -0,0 +1,35 @@
+#include <muduo/net/EventLoop.h>
+#include <boost/bind.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <muduo/base/Logging.h>
+#include <muduo/base/Thread.h>
+#include <muduo/net/EventLoop.h>
+#include <boost/bind.hpp>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <stdio.h>
+#include <sstream>
+using namespace muduo::net;
+
+EventLoop* g_loop = NULL;
+
+int main(int argc, char* argv[])
+{
+  muduo::Logger::setLogLevel(muduo::Logger::LogLevel::TRACE);
+  EventLoop loop;
+  g_loop = &loop;
+  int timerfd=timerfd_create(CLOCK_MONOTONIC,TFD_NONBLOCK|TFD_CLOEXEC);
+  muduo::Channel channel(&loop,timerfd);
+  channel.setReadCallback([&](){g_loop->quit();});
+  channel.enableReading();
+  struct itimerspec howlong;
+  bzero(&howlong,sizeof howlong);
+  howlong.it_value.tv_sec=5;
+  timerfd_settime(timerfd,0,&howlong,NULL);
+
+  // muduo::Thread t([&](){g_loop->loop();});
+  // t.start();
+  // t.join();
+  g_loop->loop();
+  close(timerfd);
+}
