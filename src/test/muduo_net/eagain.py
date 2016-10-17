56a36c747d86f01c4cdea2f673f072bb0dddb1c2
lzxm160 <lzxm160@sina.com>
2016-10-17 09:00:59 +0800

    eagin.py

--- src/test/muduo_net/eagain.py
+++ src/test/muduo_net/eagain.py
@@ -3,8 +3,8 @@ import socket,sys
 sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect(('127.0.0.1',19999))
 sock.setblocking(0)
-a='a'*1000000
-b='b'*1000000
+a='a'*100000000
+b='b'*100000000
 n1=sock.send(a)
 n2=0
 try:
