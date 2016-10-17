9b853d0a3c53e1f76e13fa53e37ca106c68ffd46
lzxm160 <lzxm160@sina.com>
2016-10-17 10:03:36 +0800

    test multi connector

---
+++ src/test/muduo_net/test_connect.cpp
@@ -0,0 +1,37 @@
+#include <muduo/net/EventLoop.h>
+#include <boost/bind.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <muduo/base/Logging.h>
+#include <muduo/base/Thread.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/Connector.h>
+#include <boost/bind.hpp>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <stdio.h>
+#include <sstream>
+using namespace muduo::net;
+
+EventLoop* g_loop = NULL;
+void onConnect(int sockfd)
+{
+	printf("connected\n");
+	g_loop->quit();
+}
+int main(int argc, char* argv[])
+{
+  muduo::Logger::setLogLevel(muduo::Logger::LogLevel::DEBUG);
+  EventLoop loop;
+  g_loop = &loop;
+  muduo::net::InetAddress addr("127.0.0.1",19999);
+  for(int i=0;i<10000;++i)
+  {
+  	  boost::scoped_ptr<muduo::net::Connector> conn(new muduo::net::Connector(&loop,addr));
+	  conn->setNewConnectionCallback(onConnect);
+	  conn->start();
+  }
+  
+  loop.loop();
+  
+  
+}
