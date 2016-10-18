ab5ba506753f7efb7cab55b95aa90017b86e9dfb
lzxm160 <lzxm160@sina.com>
2016-10-18 09:55:21 +0800

    test int64_t

--- src/test/muduo_net/trace_string.cpp
+++ src/test/muduo_net/trace_string.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <inttypes.h>
+#include <stdio.h>
+#define __STDC_FORMAT_MACROS
+
 EventLoop* g_loop = NULL;
 
 int main(int argc, char* argv[])
 {
   std::string s("xx");
+  int64_t  x=100;
+  printf("%" PRId64 "\n",x);
+  printf("%06" PRId64 "\n",x );
 }
