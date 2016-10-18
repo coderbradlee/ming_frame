42e9e73d1a2db0e79c0aae429b67f8bad1d32f38
lzxm160 <lzxm160@sina.com>
2016-10-18 16:32:11 +0800

    test backtrace

--- src/test/test3.hpp
+++ src/test/test3.hpp
@@ -772,9 +772,31 @@ namespace test3_namespace
 	        printf("%sn\n", strings[i]); 
 	    }
     }
+    const char* convert(char buf[],int value)
+    {
+    	static char digits[19]={'9','8','7','6','5','4','3','2','1','0','1','2','3','4','5','6','7','8','9'};
+    	static const char* zero=digits+9;
+    	char* p=buf;
+    	int i=value;
+    	do
+    	{
+    		int lsd=i%10;
+    		i/=10;
+    		*p++=zero[lsd];
+    	}while(i!=0);
+    	if(value<0)
+    		*p++='-';
+    	*p='\0';
+    	std::reverse(buf,strlen(buf));
+    	bt();
+    	return buf;
+    }
 	void test_out()
 	{
-		bt();
+		int value=-567890;
+		char buf[]="-567890";
+		char * p=convert(buf,value);
+		printf("%s\n",p);
 		//test_reverse();
 		// test_snprintf();
 		// test_ostringstream();
