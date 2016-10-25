4d5af8970410471d88edabc00260200e9b36b5c9
lzxm160 <lzxm160@sina.com>
2016-10-25 15:30:08 +0800

    test4

--- src/test/test4.hpp
+++ src/test/test4.hpp
@@ -103,6 +103,8 @@ namespace test4_namespace
 				delete phead;
 				phead=p;
 				p=p->next;
+				std::cout<<"::"<<prev->value<<std::endl;
+				std::cout<<"::"<<phead->value<<std::endl;
 				std::cout<<"::"<<p->value<<std::endl;
 			}
 			else
