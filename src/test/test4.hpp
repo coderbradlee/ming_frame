4d9e8b978258de047223f31ad57d146b11d67f2e
lzxm160 <lzxm160@sina.com>
2016-10-27 16:51:48 +0800

    manacher

--- src/test/test4.hpp
+++ src/test/test4.hpp
@@ -659,8 +659,8 @@ namespace test4_namespace
 			// p[i]++;
 			while(i!=p[i])
 			{
-				char prev=s[i-p[i]-1];
-				char after=s[i+p[i]-1];
+				char prev=s[i-p[i]];
+				char after=s[i+p[i]];
 				if(prev==after)
 				{
 					p[i]++;
