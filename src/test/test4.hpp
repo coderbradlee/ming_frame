2567af56af5c95db495ac2a0e86f8461f957d2e1
lzxm160 <lzxm160@sina.com>
2016-10-27 09:01:46 +0800

    permutation

--- src/test/test4.hpp
+++ src/test/test4.hpp
@@ -554,10 +554,10 @@ namespace test4_namespace
 	void test_permutation()
 	{
 		int x[]={1,2,3};
-		permutation(x,4,0);
+		permutation(x,3,0);
 		std::cout<<"-----------------"<<std::endl;
 		int y[]={2,1,3};
-		permutation(y,4,0);
+		permutation(y,3,0);
 	}
 	void test_out()
 	{
