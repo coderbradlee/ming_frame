4cac6d8fbbf3e3e065855bc15d3fed90071e4d6d
lzxm160 <lzxm160@sina.com>
2016-10-31 09:18:35 +0800

    count

--- src/test/test4.hpp
+++ src/test/test4.hpp
@@ -722,7 +722,7 @@ namespace test4_namespace
 	}
 	void test_count()
 	{
-		std::vector<std::vector<int>> graph(2 ,std::vector<int>(2,0));
+		std::vector<std::vector<int>> graph(3 ,std::vector<int>(2,0));
 		cal_count(2,graph);
 		for(int i=1;i<=2;++i)
 		{	for(int j=1;j<=2;++j)
