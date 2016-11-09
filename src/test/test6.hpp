f67c5f9e3ffb98368ec1fd0fdb9c8c6d48e55177
lzxm160 <lzxm160@sina.com>
2016-11-09 15:28:17 +0800

    sudoku

--- src/test/test6.hpp
+++ src/test/test6.hpp
@@ -299,9 +299,283 @@ namespace test6_namespace
 		std::cout<<std::endl;
 
 	}
+	enum class suit { soil,water,lake,ocean};
+	bool isOcean(const std::vector<std::vector<suit>>& graph,int row,int col,int i,int j)
+	{
+		if((i<0)||(i>=row)||(j<0)||(j>=col))
+			return false;
+		return graph[i][j]==suit::water;
+	}
+	void ocean(std::vector<std::vector<suit>>& graph,
+		int rowSize,
+		int colSize,
+		int row,
+		int col)
+	{
+		int iDirect[]={-1,1,0,0};
+		int jDirect[]={0,0,-1,1};
+		std::queue<std::pair<int,int>> q;
+		q.push(std::make_pair(row,col));
+		while(!q.empty())
+		{
+			int i=q.front().first;
+			int j=q.front().second;
+			q.pop();
+			for(int k=0;k<4;++k)
+			{
+				int iCur=i+iDirect[k];
+				int jCur=j+jDirect[k];
+				if(isOcean(graph,rowSize,colSize,iCur,jCur))
+				{
+					q.push(std::make_pair(iCur,jCur));
+					graph[iCur][jCur]=suit::ocean;
+				}
+			}
+		}
+	}
+	void fillLake(std::vector<std::vector<suit>>& graph,int rowSize,int colSize)
+	{
+		for(int i=1;i<colSize-1;++i)
+		{
+			if(graph[0][i]==suit::water)
+			{
+				graph[0][i]=suit::ocean;
+				ocean(graph,rowSize,colSize,0,i);
+			}
+			if(graph[rowSize-1][i]==suit::water)
+			{
+				graph[rowSize-1][i]=suit::ocean;
+				ocean(graph,rowSize,colSize,rowSize-1,i);
+			}
+		}
+		for(int i=0;i<rowSize;++i)
+		{
+			if(graph[i][0]==suit::water)
+			{
+				graph[i][0]=suit::ocean;
+				ocean(graph,rowSize,colSize,i,0);
+			}
+			if(graph[i][colSize-1]==suit::water)
+			{
+				graph[i][colSize-1]=suit::ocean;
+				ocean(graph,rowSize,colSize,i,colSize-1);
+			}
+		}
+		for(auto& i:graph)
+		{
+			for(auto& j:i)
+			{
+				using color_rep_type = std::underlying_type<suit>::type;
+				std::cout << static_cast<color_rep_type>(j) <<" ";
+				if(j==suit::water)
+					j=suit::soil;
+			}
+			std::cout<<std::endl;
+		}
+	}
+	void test_fillLake()
+	{
+		//把边界上的water变为ocean，遍历和它相连的water，变为ocean
+		//然后把其余的water变为soil，最后把ocean变为water
+		
+		const int N=4;
+		std::vector<std::vector<suit>> graph(N,std::vector<suit>(N,suit::soil));
+		graph[1][1]=suit::water;graph[1][2]=suit::water;
+		graph[2][2]=suit::water;graph[3][1]=suit::water;
+		fillLake(graph,4,4);
+		for(auto& i:graph)
+		{
+			for(auto& j:i)
+			{
+				using color_rep_type = std::underlying_type<suit>::type;
+				std::cout << static_cast<color_rep_type>(j) <<" ";
+			}
+			std::cout<<std::endl;
+		}
+
+	}
+	class queens
+	{
+	public:
+		queens(int n):m_num(n),
+		m_col(n,false),
+		m_mainDiagonal(2*n-1,false),
+		m_minorDiagonal(2*n-1,false){}
+		void queen()
+		{
+			std::vector<int> onePath(m_num);
+			calcNqueen(onePath,0);
+		}
+		void print()const
+		{
+			for(auto i:m_Answer)
+			{
+				for(auto j:i)
+				{
+					std::cout<<j<<" ";
+				}
+				std::cout<<std::endl;
+			}
+			std::cout<<"num of answer:"<<m_Answer.size()<<std::endl;
+		}
+	private:
+		void calcNqueen(std::vector<int>& path,int row)
+		{
+			if(row==m_num)
+			{
+				m_Answer.push_back(path);
+				return;
+			}
+			for(int i=0;i<m_num;++i)
+			{
+				if(canLay(row,i))
+				{
+					path[row]=i;
+					m_col[i]=true;
+					m_mainDiagonal[row-i+m_num-1]=true;
+					m_minorDiagonal[row+i]=true;
+					calcNqueen(path,row+1);
+					//回溯 
+					m_col[i]=false;
+					m_mainDiagonal[row-i+m_num-1]=false;
+					m_minorDiagonal[row+i]=false;
+				}
+			}
+		}
+		bool canLay(int row,int col)const
+		{
+			return !m_col[col]&&!m_minorDiagonal[row+col]&&!m_mainDiagonal[m_num-1+row-col];
+		}
+	private:
+		int m_num;
+		std::vector<bool> m_col;
+		std::vector<bool> m_mainDiagonal;
+		std::vector<bool> m_minorDiagonal;
+		std::vector<std::vector<int>> m_Answer;
+	};
+	void test_queen()
+	{
+		queens q(8);
+		q.queen();
+		q.print();
+	}
+	class sudoku
+	{
+	public:
+		sudoku(std::vector<std::vector<int>> chess):m_chess(chess)
+		{
+			rowSize=m_chess.size();
+			colSize=m_chess[0].size();
+		}
+		bool solve()
+		{
+			
+			for(int i=0;i<rowSize;++i)
+			{
+				for(int j=0;j<colSize;++j)
+				{
+					if(m_chess[i][j]==0)
+					{
+						for(int k=1;k<10;++k)
+						{
+							m_chess[i][j]=k;
+							if(isValid(i,j)&&solve())
+							{
+								m_ret.push_back(m_chess);
+							}
+						}
+						m_chess[i][j]=0;
+						return false;
+					}
+				}
+
+			}
+			return true;
+		}
+		void print()
+		{
+			for(auto& i:m_ret)
+			{
+				for(auto& j:i)
+				{
+					for(auto& x:j)
+					{
+						std::cout<<x<<" ";
+					}
+					std::cout<<std::endl;
+				}
+				std::cout<<"-------------"<<std::endl;
+			}	
+
+		}
+	private:
+		bool isValid(int row,int col)
+		{
+			for(int i=0;i<colSize;++i)
+			{
+				if((m_chess[row][i]==m_chess[row][col])&&i!=col)
+					return false;
+			}
+			for(int i=0;i<rowSize;++i)
+			{
+				if((m_chess[i][col]==m_chess[row][col])&&i!=row)
+					return false;
+			}
+			//
+			int rowIndex=row/3*3;
+			int colIndex=col/3*3;
+			for(int i=rowIndex;i<rowIndex+3;++i)
+			{
+				for(int j=colIndex;j<colIndex+3;++j)
+				{
+					if((row!=i)&&(col!=j)&&(m_chess[i][j]==m_chess[row][col]))
+						return false;
+				}
+			}
+			return true;
+		}
+	private:
+		std::vector<std::vector<int>> m_chess;
+		int rowSize;
+		int colSize;
+		std::vector<std::vector<std::vector<int>>> m_ret;
+	};
+	void test_sudoku()
+	{
+		// std::vector<std::vector<int>> chess=
+		// {
+		// 	{0, 4, 2, 0, 6, 3, 0, 0, 9},
+		// 	{6, 0, 0, 0, 1, 0, 0, 0, 5},
+		// 	{3, 0, 0, 0, 2, 0, 4, 8, 0},
+		// 	{1, 0, 0, 5, 0, 2, 6, 0, 8},
+		// 	{4, 0, 0, 0, 0, 7, 0, 0, 1},
+		// 	{9, 0, 5, 6, 0, 0, 0, 0, 7},
+		// 	{0, 3, 6, 0, 5, 0, 0, 0, 2},
+		// 	{2, 0, 0, 0, 7, 0, 0, 0, 4},
+		// 	{7, 0, 0, 2, 9, 0, 8, 5, 0}
+		// };
+		std::vector<std::vector<int>> chess=
+		{
+			{0,0,0,0,0,0,0,1,0},
+			{4,0,0,0,0,0,0,0,0},
+			{0,2,0,0,0,0,0,0,0},
+			{0,0,0,0,5,0,4,0,7},
+			{0,0,8,0,0,0,3,0,0},
+			{0,0,1,0,9,0,0,0,0},
+			{3,0,0,4,0,0,2,0,0},
+			{0,5,0,1,0,0,0,0,0},
+			{0,0,0,8,0,6,0,0,0}
+		};
+		sudoku s(chess);
+		s.solve();
+		s.print();
+	}
 	void test_out()
 	{
-		test_handred();
+		test_sudoku();
+		// test_queen();
+		//test_fillLake();
+		// test_handred();
 		//std::cout<<"1"<<std::endl;
 		// test_mouseCheese();
 		//std::cout<<"2"<<std::endl;
