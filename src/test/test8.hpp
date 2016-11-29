0ecfd919b88355d94825444884d59dd3bf050420
lzxm160 <lzxm160@sina.com>
2016-11-29 20:18:38 +0800

    horse jump

--- src/test/test8.hpp
+++ src/test/test8.hpp
@@ -1290,8 +1290,8 @@ void test_horseJump()
 {
 	// int m=5;
 	// int n=6;
-	int m=8;
-	int n=8;
+	int m=6;
+	int n=7;
 	std::vector<std::vector<int>> chess(m,std::vector<int>(n,0));
 	int step=1;
 	chess[0][0]=1;
