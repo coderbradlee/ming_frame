86ae1cfd6ac0ccc65e7cce20e920235c30ea07ed
lzxm160 <lzxm160@sina.com>
2016-11-29 20:13:19 +0800

    horse jump

--- src/test/test8.hpp
+++ src/test/test8.hpp
@@ -1288,12 +1288,15 @@ bool horseJump(std::vector<std::vector<int>>& chess,int row,int col,int step)
 }
 void test_horseJump()
 {
-	int m=5;
-	int n=6;
+	// int m=5;
+	// int n=6;
+	int m=8;
+	int n=8;
 	std::vector<std::vector<int>> chess(m,std::vector<int>(n,0));
 	int step=1;
 	chess[0][0]=1;
-	horseJump(chess,0,0,step);
+	std::cout<<horseJump(chess,0,0,step)<<std::endl;
+
 	for(auto& i:chess)
 	{
 		for(auto& j:i)
